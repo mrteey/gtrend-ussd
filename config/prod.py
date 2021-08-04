5ba111e688fe57ad29e9640427b3489c4f6c551a
Nasir Mustapha <teboy.com@gmail.com>
2021-08-04 01:48:40 +0100

    updated prod vars

--- config/prod.py
+++ config/prod.py
@@ -1,2 +1,3 @@
 import os
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
+SQLALCHEMY_TRACK_MODIFICATIONS = True
